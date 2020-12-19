a15184d2d32f7327c2957cbdeb2bc1a588391b95
朱伟伟 <596177831@qq.com>
2020-12-20 00:36:25 +0800

    retrofit调用文件测试

--- src/main/java/com/example/okhttp/retrofit/GitHubService.java
+++ src/main/java/com/example/okhttp/retrofit/GitHubService.java
@@ -2,12 +2,16 @@ package com.example.okhttp.retrofit;
 
 import com.example.okhttp.retrofit.entity.ScenicHttpResult;
 import com.example.okhttp.retrofit.entity.ScenicList;
+import okhttp3.FormBody;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.http.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,14 +20,32 @@ import java.util.Map;
  * @description
  */
 public interface GitHubService {
+    /**
+     * @param supplierIdentity:
+     * @param signkey:
+     * @author: 朱伟伟
+     * @date: 2020-12-20 00:34
+     * @description: application/x-www-form-urlencoded form-data 格式
+     **/
     @FormUrlEncoded
     @POST("getScenicList")
     Call<ScenicHttpResult<ScenicList>> getScenicList(@Field("supplierIdentity") String supplierIdentity, @Field("signkey") String signkey);
 
     @FormUrlEncoded
     @POST("getScenicList")
-    Call<ResponseBody> getStringScenicList(@Field("supplierIdentity") String supplierIdentity, @Field("signkey") String signkey);
+    Call<ResponseBody> getResponseBodyScenicList(@Field("supplierIdentity") String supplierIdentity, @Field("signkey") String signkey);
 
+    /**
+     * @param testPath:
+     * @param bodyJson:
+     * @param projectId:
+     * @param queryMap:
+     * @param accessKey:
+     * @param headerMap:
+     * @author: 朱伟伟
+     * @date: 2020-12-20 00:34
+     * @description: body json 格式
+     **/
     @Headers({
             "signKey:147895336",
     })
@@ -35,7 +57,14 @@ public interface GitHubService {
                                                         @Header("accessKey") String accessKey,
                                                         @HeaderMap Map<String, String> headerMap);
 
+    /**
+     * @param parts:
+     * @author: 朱伟伟
+     * @date: 2020-12-20 00:35
+     * @description: multipart/form-data格式
+     **/
     @Multipart
-    @PUT("retrofitMultipart")
-    Call<ScenicHttpResult<ScenicList>> retrofitMultipart(@Part(value = "idPic") RequestBody idPic);
+    @PUT("retrofitMultipartBodyFile")
+    Call<ScenicHttpResult<ScenicList>> retrofitMultipartBodyFile(@Part List<MultipartBody.Part> parts);
+
 }
--- src/main/java/com/example/okhttp/retrofit/RetrofitController.java
+++ src/main/java/com/example/okhttp/retrofit/RetrofitController.java
@@ -5,9 +5,11 @@ import com.alibaba.fastjson.support.retrofit.Retrofit2ConverterFactory;
 import com.example.okhttp.retrofit.entity.ScenicHttpResult;
 import com.example.okhttp.retrofit.entity.ScenicList;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -15,6 +17,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author 朱伟伟
@@ -31,8 +34,11 @@ public class RetrofitController implements InitializingBean {
     public final String signkey = "3db0dc1a3dc6821bd0e7a5f772a73c0b";
     private Retrofit retrofit;
 
-//    @Autowired
-//    ScenicHttpService scenicHttpService;
+    private final Retrofit localRetrofit = new Retrofit.Builder()
+            .baseUrl("http://127.0.0.1:8082/demo/retrofit/")
+            .addConverterFactory(new Retrofit2ConverterFactory())
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build();
 
     @GetMapping("/getScenicList")
     public ScenicHttpResult<ScenicList> getScenicList() throws IOException {
@@ -40,7 +46,7 @@ public class RetrofitController implements InitializingBean {
         Response<ScenicHttpResult<ScenicList>> response = gitHubService.getScenicList(supplierIdentity, signkey).execute();
         if (response.isSuccessful()) {
             ScenicHttpResult<ScenicList> scenicHttpResult = response.body();
-            System.out.println(scenicHttpResult);
+//            System.out.println(scenicHttpResult);
             return scenicHttpResult;
         }
         return null;
@@ -49,7 +55,7 @@ public class RetrofitController implements InitializingBean {
     @GetMapping("/getResponseBodyScenicList")
     public String getResponseBodyScenicList() throws IOException {
         GitHubService gitHubService = retrofit.create(GitHubService.class);
-        Response<okhttp3.ResponseBody> response = gitHubService.getStringScenicList(supplierIdentity, signkey).execute();
+        Response<okhttp3.ResponseBody> response = gitHubService.getResponseBodyScenicList(supplierIdentity, signkey).execute();
         if (response.isSuccessful()) {
             MediaType mediaType = response.body().contentType();
             System.out.println(mediaType);
@@ -69,14 +75,16 @@ public class RetrofitController implements InitializingBean {
                 .build();
     }
 
+
+    /**
+     * @author: 朱伟伟
+     * @date: 2020-12-19 23:16
+     * @description: retrofit 测试
+     **/
+
     @GetMapping("/getBodyJson")
     public ScenicHttpResult<ScenicList> getBodyJson() throws IOException {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://127.0.0.1:8082/demo/retrofit/")
-                .addConverterFactory(new Retrofit2ConverterFactory())
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
-        GitHubService gitHubService = retrofit.create(GitHubService.class);
+        GitHubService gitHubService = localRetrofit.create(GitHubService.class);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("name", "朱伟伟");
         jsonObject.put("email", "596177831@qq.com");
@@ -113,5 +121,33 @@ public class RetrofitController implements InitializingBean {
         System.out.println("RequestHeader Authorization：" + Authorization);
         return getScenicList();
     }
+
+    /**
+     * @author: 朱伟伟
+     * @date: 2020-12-19 23:22
+     * @description: retrofit文件测试接口
+     **/
+
+    @GetMapping("/getRetrofitFile")
+    public ScenicHttpResult<ScenicList> getRetrofitFile() throws IOException {
+        GitHubService gitHubService = localRetrofit.create(GitHubService.class);
+        File file = new File("D:/闫盼盼《 甜蜜的俘虜 》/aHR0cHM6Ly93d3cubXlteXBpYy5uZXQvZGF0YS9hdHRhY2htZW50L2ZvcnVtLzIwMTkwNC8yMS8wMDQ3MTJkbm1jbHV0eXMxa21ubmN0LmpwZy50aHVtYi5qcGc=.jpg");
+        okhttp3.RequestBody fileRequestBody = okhttp3.RequestBody.create(null, file);
+        List<MultipartBody.Part> parts = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("studentName", "朱伟伟")
+                .addFormDataPart("imageFile", file.getPath(), fileRequestBody)
+                .build().parts();
+        return gitHubService.retrofitMultipartBodyFile(parts).execute().body();
+    }
+
+    @PutMapping("/retrofitMultipartBodyFile")
+    public ScenicHttpResult<ScenicList> retrofitMultipartBodyFile(String studentName, MultipartFile imageFile) throws IOException {
+        System.out.println("studentName：" + studentName);
+        System.out.println("图片文件名称：" + imageFile.getOriginalFilename());
+        System.out.println("图片文件大小：" + imageFile.getSize());
+        return getScenicList();
+    }
+
 
 }
