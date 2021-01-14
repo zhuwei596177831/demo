618930cfbe5860dfe15d99dc53f98fb21870bfb0
zhuweiwei <596177831@qq.com>
2021-01-14 18:22:13 +0800

    HandlerMethodArgumentResolver 测试

--- src/main/java/com/example/validation/ExceptionControllerAdvice.java
+++ src/main/java/com/example/validation/ExceptionControllerAdvice.java
@@ -1,10 +1,14 @@
 package com.example.validation;
 
+import org.springframework.core.MethodParameter;
 import org.springframework.validation.BindException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.validation.ConstraintViolationException;
 
@@ -35,12 +39,26 @@ public class ExceptionControllerAdvice {
         return new Result<>(ResultCode.VALIDATE_FAILED, e.getMessage(), null);
     }
 
+    /**
+     * @param e:
+     * @author: 朱伟伟
+     * @date: 2021-01-14 18:21
+     * @description:
+     * @see org.springframework.web.servlet.mvc.method.annotation.ServletModelAttributeMethodProcessor#resolveArgument
+     **/
     @ExceptionHandler(BindException.class)
     public Result MethodArgumentNotValidExceptionHandler(BindException e) {
         ObjectError objectError = e.getBindingResult().getAllErrors().get(0);
         return new Result<>(ResultCode.VALIDATE_FAILED, objectError.getDefaultMessage(), null);
     }
 
+    /**
+     * @param e:
+     * @author: 朱伟伟
+     * @date: 2021-01-14 18:19
+     * @description:
+     * @see org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor#resolveArgument
+     **/
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Result MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         ObjectError objectError = e.getBindingResult().getAllErrors().get(0);
